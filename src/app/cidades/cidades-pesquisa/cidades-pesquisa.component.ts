b9d8116a361a53ce8f70d77d4d542c94e77fbf27
Leonardo Silva <leonardo.silva1303@etec.sp.gov.br>
2020-03-16 10:57:27 -0300

    Aula 1603

--- src/app/cidades/cidades-pesquisa/cidades-pesquisa.component.ts
+++ src/app/cidades/cidades-pesquisa/cidades-pesquisa.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CidadeFiltro, CidadesService } from '../cidades.service';
+import { LazyLoadEvent } from 'primeng/api';
 
 @Component({
   selector: 'app-cidades-pesquisa',
@@ -10,16 +11,29 @@ export class CidadesPesquisaComponent implements OnInit {
 
   totalRegistros = 0;
   filtro = new CidadeFiltro();
-  cidades = [  ];
+  cidades = [];
 
 
   constructor(private cidadesService: CidadesService) { }
 
   ngOnInit() {
+    this.pesquisar();
   }
 
-  pesquisar(pagina=0) {
+  pesquisar(pagina = 0) {
     this.filtro.pagina = pagina;
+
+    this.cidadesService.pesquisar(this.filtro)
+      .then(resultado => {
+        this.totalRegistros = resultado.total;
+        this.cidades = resultado.cidades;
+        console.log(this.cidades);
+      });
+  }
+
+  aoMudarPagina(event: LazyLoadEvent){
+    const pagina = event.first/event.rows;
+    this.pesquisar(pagina);
   }
 
 }
